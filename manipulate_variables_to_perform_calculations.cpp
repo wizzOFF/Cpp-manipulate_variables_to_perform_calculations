87af3ce6fdb3cdc6e425bc337dc2f66af9805926
WizzOFF <maher_off@proton.me>
2025-03-26 23:50:21 +0100

    manipulate_variables_to_perform_calculations.cpp: getline() to display a string

--- manipulate_variables_to_perform_calculations.cpp
+++ manipulate_variables_to_perform_calculations.cpp
@@ -5,14 +5,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Quel est votre prenom ?" << endl;
-    string nomUtilisateur("Sans nom"); //On crée une case mémoire pour contenir une chaine de caractères
-    cin >> nomUtilisateur; //On remplit cette case avec ce qu'écrit l'utilisateur
-
     cout << "Combien vaut pi ?" << endl;
-    double piUtilisateur(-1.); //On crée une case mémoire pour stocker un nombre réel
-    cin >> piUtilisateur; //Et on remplit cette case avec ce qu'écrit l'utilisateur
+    double piUtilisateur(-1.); //On crée une case mémoire pour stocker unnombre réel
+    cin >> piUtilisateur; //Et on remplit cette case avec ce qu'écritl'utilisateur
 
+    cin.ignore();
+
+    cout << "Quel est votre nom ?" << endl;
+    string nomUtilisateur("Sans nom"); //On crée une case mémoire pour contenir une chaine de caractères
+    getline(cin, nomUtilisateur); //On remplit cette case avec toute la ligne que l'utilisateur a écrit
+     
     cout << "Vous vous appelez " << nomUtilisateur << " et vous pensez que pi vaut " << piUtilisateur << "." << endl;
 
     return 0;
