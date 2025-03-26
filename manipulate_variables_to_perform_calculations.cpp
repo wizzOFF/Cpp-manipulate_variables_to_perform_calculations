42ce12aebb9625016dd734868f3eb045800719dc
WizzOFF <maher_off@proton.me>
2025-03-26 23:06:33 +0100

    manipulate_variables_to_perform_calculations.cpp: store information given by the user > string

--- manipulate_variables_to_perform_calculations.cpp
+++ manipulate_variables_to_perform_calculations.cpp
@@ -1,16 +1,19 @@
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << "Quel age avez-vous ?" << endl;
+    cout << "Quel est votre prenom ?" << endl;
+    string nomUtilisateur("Sans nom"); //On crée une case mémoire pour contenir une chaine de caractères
+    cin >> nomUtilisateur; //On remplit cette case avec ce qu'écrit l'utilisateur
 
-    int ageUtilisateur(0); //On prépare une case mémoire pour stocker un entier
+    cout << "Combien vaut pi ?" << endl;
+    double piUtilisateur(-1.); //On crée une case mémoire pour stocker un nombre réel
+    cin >> piUtilisateur; //Et on remplit cette case avec ce qu'écrit l'utilisateur
 
-    cin >> ageUtilisateur; //On fait entrer un nombre dans cette case
-
-    cout << "Vous avez " << ageUtilisateur << " ans !" <<  endl; //Et on l'affiche
+    cout << "Vous vous appelez " << nomUtilisateur << " et vous pensez que pi vaut " << piUtilisateur << "." << endl;
 
     return 0;
 }
