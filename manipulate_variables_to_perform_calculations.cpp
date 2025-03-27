7d0209751a0c96f6c2ffcad2535f5a149fc06680
WizzOFF <maher_off@proton.me>
2025-03-27 01:21:08 +0100

    manipulate_variables_to_perform_calculations.cpp: declare an unmodifiable variable const

--- manipulate_variables_to_perform_calculations.cpp
+++ manipulate_variables_to_perform_calculations.cpp
@@ -45,10 +45,12 @@ using namespace std;
 int main()  
 {
   int resultat(0), a(5), b(8);
+  
+  int const nombreNiveaux(10);
 
   resultat = a + b;
 
   cout << "5 + 8 = " << resultat << endl;
-  
+
   return 0;
 }
